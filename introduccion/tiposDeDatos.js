af353b39948806ebdea4738150235bef35eb50eb
paul salazar <70818490+paulsalazar24@users.noreply.github.com>
2023-05-19 21:44:52 -0500

    avance dia 1

---
+++ introduccion/tiposDeDatos.js
@@ -0,0 +1,77 @@
+/**
+ * TIPOS DE DATOS
+ * 
+ * string  =  cadena de texto
+ * number = numero
+ * boolean  = boleano (verdadero o falso)
+ * object = objeto
+ * 
+ * 
+ * 
+ * null = valor nulo
+ * undefined = valor sin definir
+ * 
+ */
+
+//cadena de texto - se puede trabajar con comillas simples o dobles
+let cadena = 'paul neder';
+const direccion = 'los olivos';
+const parrafo = "lima - san isidro";
+
+//numero
+
+const numero = 4;
+const numero2 = -2;
+
+//boleanon - ayuda a las condiciones con el uso de los operadores
+
+const accion = true;
+const usuarioConectado = false;
+
+//array - arreglos
+const numeros = [1,2,3,4,5];
+const nombres = ['paul','jose','loin'];
+const combinado = ['paul', 112 , {propiedad: 'valor'},[1,2,3,4]];
+
+
+// objeto - propiedad y valor
+
+const persona = {
+     nombre: 'paul',
+     apellido : 'salazar', 
+     edad: 23,
+     carro:{
+        marca:'bmb',
+        color:'rojo',
+     },
+    };
+
+// function - se debe de inicializar una funcion para poder acceder a la 
+//información 
+
+function hola(){
+    console.log('Hola');
+}
+
+hola();
+
+
+//undefined - valor no definido
+
+const miValor = undefined;
+
+
+//null
+
+const miValorNuevo = null; 
+
+
+
+
+
+
+
+
+
+
+
--- introduccion/variables.js
+++ introduccion/variables.js
@@ -1,2 +1,13 @@
 let edad = 25;
+var edad1 = 23;
 
+
+const nombre1 = 'carlos';
+const nombre2 = 'juan';
+const nombreCompleto = nombre1 + nombre2;
+console.log(nombreCompleto);
+
+
+//la variable puede cambiar de valor en caso se deseo modificar 
+let miVariable = 'texto';
+miVariable = true;
