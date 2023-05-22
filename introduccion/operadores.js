14e02d22e09191ecab4b2186f8f6597c4a7a5f80
paulsalazar24 <70818490+paulsalazar24@users.noreply.github.com>
2023-05-22 16:55:10 -0500

    operadores

---
+++ introduccion/operadores.js
@@ -0,0 +1,76 @@
+/*
+PERADORES ARITMETICOS
+    = OPERADOR DE ASIGNACION 
+    + SUMA 
+    - RESTA
+    * MULTIPLICACION
+    / DIVISION
+    % MODULO
+    ++ AUMENTO
+    -- DISMINUCION
+
+*/
+
+const resultado = 10 + 10;
+const resultado2 = 10 - 10;
+const resultado3 = 10 % 3;
+
+let numero = 1;
+numero++;
+
+console.log(numero);
+
+
+
+/*
+OPERADORES DE ASIGNACION
+        += SEMA UN NUMERO AL VALOR DE UNA VARIABLES
+        -= RESTA UN NUMERO AL VALOR DE UNA VARIABLES
+        *= RESTA UN NUMERO AL VALOR DE UNA VARIABLE
+        /= RESTA UN NUMERO AL VALOR DE UNA VARIABLE
+        %= OBTIENE EL SOBRANTE DE UNA DIVISION Y LA ASIGNO A LA VARIABLES
+*/
+
+let numeroPol = 10
+// numeroPol = numeroPol + 5;
+numero -=5;
+
+console.log(numeroPol);
+
+
+
+/*
+OPERADORES DE COMPARACION
+NOS PERMITEN COMPRAR VALORES
+
+        == IGUAL QUE 
+        === IGUAL EN VALOR Y TIPO DE VALOR
+        != DIFERENTE
+        !== DIFERENTE VALOR Y DIFERENTE TIPO
+        < MENOR QUE
+        > MAYOR QUE
+        <= MENOR O IGUAL QUE
+        >= MAYOR O IGUAL QUE 
+        ? OPERADOR TERNARIO
+
+*/
+
+const pruebaCom = 5>1; //true
+
+console.log(pruebaCom);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
